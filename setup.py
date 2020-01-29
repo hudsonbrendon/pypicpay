3b5c82949798ea57174e9309c300dbde95abc99b
Hudson Brendon <contato.hudsonbrendon@gmail.com>
2020-01-28 22:35:25 -0300

    Added base project

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+
+setup(
+    name="picpay-python",
+    version="0.1",
+    description="Aceite PicPay e faça parte do movimento que está revolucionando a relação com o dinheiro no Brasil.",
+    url="https://github.com/hudsonbrendon/picpay-python",
+    author="Hudson Brendon",
+    author_email="contato.hudsonbrendon@gmail.com",
+    license="MIT",
+    packages=["picpay"],
+    install_requires=["requests",],
+    zip_safe=False,
+)
