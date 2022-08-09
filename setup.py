cc5e2ca12bd2ddd2f965e6e560c8b6d819c39034
Hudson Brendon <contato.hudsonbrendon@gmail.com>
2022-08-09 19:29:43 -0300

    refactor: update project version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="picpay-python",
-    version="0.4.1",
+    version="0.4.2",
     description="Aceite PicPay e faça parte do movimento que está revolucionando a relação com o dinheiro no Brasil.",
     long_description_content_type="text/markdown",
     long_description=long_description,
